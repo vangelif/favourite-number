a436cf72a80ad45bb83ca2ded65a8d2d99d90d47
Vangelis <ev.fotiades@yahoo.com>
2023-05-01 02:40:21 +0300

    modify readme

--- main.cpp
+++ main.cpp
@@ -6,5 +6,6 @@ int favourite_number;
 cout<< "Enter your favourite number: ";
 cin>> favourite_number;
 cout<< "Amazing that is also my favourite!!"<<endl;
+cout<< "No really!! "<<favourite_number<<" is my favourite number!"<<endl;
 return 0;
 }
