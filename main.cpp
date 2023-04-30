4d557a0ab3c3f2c7d5468532a6b6f3c480776c35
Vangelis <ev.fotiades@yahoo.com>
2023-05-01 01:57:02 +0300

    add fav number

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int favourite_number;
+cout<< "Enter your favourite number: ";
+cin>> favourite_number;
+cout<< "Amazing that is also my favourite!!"<<endl;
+return 0;
+}
